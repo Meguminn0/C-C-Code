4895658427d58a47de2dbb9c24a3a9eb45e97ee9
Loser00000 <3036537400@qq.com>
2021-09-03 11:57:18 +0800

    加减乘除

---
+++ 加减乘除.c
@@ -0,0 +1,28 @@
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	float num1;
+//	float num2;
+//	float sum;
+//	float a;
+//	float b;
+//	float x;
+//
+//	printf("请输入您要计算的两个数字：");
+//	scanf_s("%f,%f",&num1,&num2);
+//
+//	sum = num1 + num2;
+//	printf("相加等于：%f\n",sum);
+//
+//	a = num1 - num2;
+//	printf("相减等于：%f\n",a);
+//
+//	b = num1 * num2;
+//	printf("相乘等于：%f\n",b);
+//
+//	x = num1 / num2;
+//	printf("相除等于：%f\n",x);
+//
+//	return 0;
+//}
